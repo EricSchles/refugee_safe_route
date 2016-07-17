818bbc92bf51415e2e324266890ace286b62d40c
rosie s <rshimada@csc.com>
2016-07-16 22:27:21 -0400

    added database config

--- code/map_app/app/__init__.py
+++ code/map_app/app/__init__.py
@@ -3,9 +3,9 @@ from flask.ext.script import Manager
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.migrate import Migrate, MigrateCommand
 
-username,password = "eric_s","1234"
+username,password = "hackathon_user","1234"
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://"+username+":"+password+"@localhost/backpage_ads"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://"+username+":"+password+"@localhost/refugee_safe_route"
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
 
